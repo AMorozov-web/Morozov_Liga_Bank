f7b9a97857c93fb16d80b492edce1dd5c3ba5bc8
AMorozov-web <AMorozov-web@users.noreply.github.com>
2021-05-20 15:50:26 +0300

    chore: prepare env to dev

---
+++ src/components/app/app.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import '../../sass/style.scss';
+
+const App = () => {
+
+  return (
+    <>
+      <div className="new">
+        NEW REACT PROJECT
+      </div>
+      <div className="visually-hidden">
+        This must be hidden
+      </div>
+    </>
+  );
+};
+
+export {App};
