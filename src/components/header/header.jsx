8cfd805787ef7a525f5110fff04928c842f77a66
Alex Morozov <a.morozov.work@mail.ru>
2021-05-22 23:04:41 +0300

    feat: create header component

---
+++ src/components/header/header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Logo} from '../logo/logo';
+import {Navigation} from '../navigation/navigation';
+import {UserBlock} from '../user-block/user-block';
+
+const Header = () => {
+
+  return (
+    <section className="header page-body__header">
+      <div className="header__container">
+        <Logo />
+        <Navigation />
+        <UserBlock />
+      </div>
+    </section>
+  );
+};
+
+export {Header};
