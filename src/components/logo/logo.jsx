6fef07d444987e0f3c8570efb792b51ec8db905e
Alex Morozov <a.morozov.work@mail.ru>
2021-05-22 23:05:14 +0300

    feat: create logo component

---
+++ src/components/logo/logo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Logo = () => {
+
+  return (
+    <a href="" className="logo header__logo">
+      <div className="logo__wrapper">
+        <img className="logo__image" src="./img/logo.svg" alt="Liga-Bank logo" />
+      </div>
+    </a>
+  );
+};
+
+export {Logo};
