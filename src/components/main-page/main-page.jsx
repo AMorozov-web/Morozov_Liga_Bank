daf641b3e6fe4f8fab90506a0924976000f9dfe5
Alex Morozov <a.morozov.work@mail.ru>
2021-05-22 23:04:18 +0300

    feat: create main-page component

---
+++ src/components/main-page/main-page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Header} from '../header/header';
+
+const MainPage = () => {
+
+  return (
+    <body className="page-body">
+      <Header />
+    </body>
+  );
+};
+
+export {MainPage};
