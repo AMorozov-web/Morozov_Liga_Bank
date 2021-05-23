c6e6c0ad271b3b3426739605fe43363133b0f023
Alex Morozov <a.morozov.work@mail.ru>
2021-05-23 23:22:41 +0300

    feat: complete footer layout

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Header} from '../header/header';
+import {Footer} from '../footer/footer';
 
 import './main-page';
 
@@ -8,6 +9,7 @@ const MainPage = () => {
   return (
     <body className="page-body">
       <Header />
+      <Footer />
     </body>
   );
 };
