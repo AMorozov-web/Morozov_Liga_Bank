1e61327f4e1e49562e5d0aa3138de5dc27e6feb9
Alex Morozov <a.morozov.work@mail.ru>
2021-05-23 18:57:52 +0300

    feat: add hovers and focuses to header

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -5,7 +5,7 @@ import './logo.scss';
 const Logo = () => {
 
   return (
-    <a href="" className="logo header__logo">
+    <a href="" className="logo header__logo" aria-label="Go to main page">
       <div className="logo__wrapper">
         <img className="logo__image" src="img/logo.svg" alt="Liga-Bank logo" />
       </div>
