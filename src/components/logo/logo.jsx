e941ac6de77166862f3cd8b405de8804980cb3ff
Alex Morozov <a.morozov.work@mail.ru>
2021-05-23 20:04:15 +0300

    feat: create universal logo component

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './logo.scss';
 
-const Logo = () => {
+const getClassName = (position) => {
+  switch (position) {
+    case `header`:
+      return `header__logo`;
+    case `footer`:
+      return `footer__logo`;
+  }
+
+  return ``;
+};
+
+const Logo = ({position}) => {
 
   return (
-    <a href="" className="logo header__logo" aria-label="Go to main page">
+    <a href="" className={`logo ${getClassName(position)}`} aria-label="Go to main page">
       <div className="logo__wrapper">
         <img className="logo__image" src="img/logo.svg" alt="Liga-Bank logo" />
       </div>
     </a>
   );
+};
+
+Logo.propTypes = {
+  position: PropTypes.string.isRequired
 };
 
 export {Logo};
