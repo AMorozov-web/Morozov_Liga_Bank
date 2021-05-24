1851e84837c2d5678ba5a71505f3a0dcbb31d034
Alex Morozov <a.morozov.work@mail.ru>
2021-05-25 01:04:03 +0300

    feat: create main component

--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Header} from '../header/header';
+import {Main} from '../main/main';
 import {Footer} from '../footer/footer';
 
 import './main-page';
@@ -9,6 +10,7 @@ const MainPage = () => {
   return (
     <>
       <Header />
+      <Main />
       <Footer />
     </>
   );
