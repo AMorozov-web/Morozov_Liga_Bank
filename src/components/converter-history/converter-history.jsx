4e82989a9acd89b59216f501ce67d072cfd3999e
AMorozov-web <AMorozov-web@users.noreply.github.com>
2021-05-25 16:02:47 +0300

    feat: complete main-component layout

---
+++ src/components/converter-history/converter-history.jsx
@@ -0,0 +1,135 @@
+import React from 'react';
+
+import './converter-history.scss';
+
+const ConverterHistory = () => {
+
+  return (
+    <section className='converter-history converter__converter-history'>
+      <div className="converter-history__container">
+        <h2 className="converter-history__title">
+          История конвертация
+        </h2>
+        <div className="converter-history__list">
+          <ul className="converter-history__list-column converter-history__list-column--left">
+            <li className="converter-history__item">
+              <p className="converter-history__date">25.11.2020</p>
+              <p className="converter-history__start-sum">1000 RUB</p>
+              <svg className="converter-history__item-icon" width="41" height="18" viewBox="0 0 41 18" fill="none">
+                <path d="M27.2 17L40 9L27.2 1" stroke="#1F1E25"/>
+                <path d="M40 9C34.6667 9 15.7407 9 6.66667 9H0" stroke="#1F1E25"/>
+              </svg>
+              <p className="converter-history__result-sum">13,1234 USD</p>
+            </li>
+            <li className="converter-history__item">
+              <p className="converter-history__date">25.11.2020</p>
+              <p className="converter-history__start-sum">1000 RUB</p>
+              <svg className="converter-history__item-icon" width="41" height="18" viewBox="0 0 41 18" fill="none">
+                <path d="M27.2 17L40 9L27.2 1" stroke="#1F1E25"/>
+                <path d="M40 9C34.6667 9 15.7407 9 6.66667 9H0" stroke="#1F1E25"/>
+              </svg>
+              <p className="converter-history__result-sum">13,1234 USD</p>
+            </li>
+            <li className="converter-history__item">
+              <p className="converter-history__date">25.11.2020</p>
+              <p className="converter-history__start-sum">1000 RUB</p>
+              <svg className="converter-history__item-icon" width="41" height="18" viewBox="0 0 41 18" fill="none">
+                <path d="M27.2 17L40 9L27.2 1" stroke="#1F1E25"/>
+                <path d="M40 9C34.6667 9 15.7407 9 6.66667 9H0" stroke="#1F1E25"/>
+              </svg>
+              <p className="converter-history__result-sum">13,1234 USD</p>
+            </li>
+            <li className="converter-history__item">
+              <p className="converter-history__date">25.11.2020</p>
+              <p className="converter-history__start-sum">1000 RUB</p>
+              <svg className="converter-history__item-icon" width="41" height="18" viewBox="0 0 41 18" fill="none">
+                <path d="M27.2 17L40 9L27.2 1" stroke="#1F1E25"/>
+                <path d="M40 9C34.6667 9 15.7407 9 6.66667 9H0" stroke="#1F1E25"/>
+              </svg>
+              <p className="converter-history__result-sum">13,1234 USD</p>
+            </li>
+            <li className="converter-history__item">
+              <p className="converter-history__date">25.11.2020</p>
+              <p className="converter-history__start-sum">1000 RUB</p>
+              <svg className="converter-history__item-icon" width="41" height="18" viewBox="0 0 41 18" fill="none">
+                <path d="M27.2 17L40 9L27.2 1" stroke="#1F1E25"/>
+                <path d="M40 9C34.6667 9 15.7407 9 6.66667 9H0" stroke="#1F1E25"/>
+              </svg>
+              <p className="converter-history__result-sum">13,1234 USD</p>
+            </li>
+            <li className="converter-history__item">
+              <p className="converter-history__date">25.11.2020</p>
+              <p className="converter-history__start-sum">1000 RUB</p>
+              <svg className="converter-history__item-icon" width="41" height="18" viewBox="0 0 41 18" fill="none">
+                <path d="M27.2 17L40 9L27.2 1" stroke="#1F1E25"/>
+                <path d="M40 9C34.6667 9 15.7407 9 6.66667 9H0" stroke="#1F1E25"/>
+              </svg>
+              <p className="converter-history__result-sum">13,1234 USD</p>
+            </li>
+          </ul>
+          <ul className="converter-history__list-column converter-history__list-column--right">
+            <li className="converter-history__item">
+              <p className="converter-history__date">25.11.2020</p>
+              <p className="converter-history__start-sum">1000 RUB</p>
+              <svg className="converter-history__item-icon" width="41" height="18" viewBox="0 0 41 18" fill="none">
+                <path d="M27.2 17L40 9L27.2 1" stroke="#1F1E25"/>
+                <path d="M40 9C34.6667 9 15.7407 9 6.66667 9H0" stroke="#1F1E25"/>
+              </svg>
+              <p className="converter-history__result-sum">13,1234 USD</p>
+            </li>
+            <li className="converter-history__item">
+              <p className="converter-history__date">25.11.2020</p>
+              <p className="converter-history__start-sum">1000 RUB</p>
+              <svg className="converter-history__item-icon" width="41" height="18" viewBox="0 0 41 18" fill="none">
+                <path d="M27.2 17L40 9L27.2 1" stroke="#1F1E25"/>
+                <path d="M40 9C34.6667 9 15.7407 9 6.66667 9H0" stroke="#1F1E25"/>
+              </svg>
+              <p className="converter-history__result-sum">13,1234 USD</p>
+            </li>
+            <li className="converter-history__item">
+              <p className="converter-history__date">25.11.2020</p>
+              <p className="converter-history__start-sum">1000 RUB</p>
+              <svg className="converter-history__item-icon" width="41" height="18" viewBox="0 0 41 18" fill="none">
+                <path d="M27.2 17L40 9L27.2 1" stroke="#1F1E25"/>
+                <path d="M40 9C34.6667 9 15.7407 9 6.66667 9H0" stroke="#1F1E25"/>
+              </svg>
+              <p className="converter-history__result-sum">13,1234 USD</p>
+            </li>
+            <li className="converter-history__item">
+              <p className="converter-history__date">25.11.2020</p>
+              <p className="converter-history__start-sum">1000 RUB</p>
+              <svg className="converter-history__item-icon" width="41" height="18" viewBox="0 0 41 18" fill="none">
+                <path d="M27.2 17L40 9L27.2 1" stroke="#1F1E25"/>
+                <path d="M40 9C34.6667 9 15.7407 9 6.66667 9H0" stroke="#1F1E25"/>
+              </svg>
+              <p className="converter-history__result-sum">13,1234 USD</p>
+            </li>
+            <li className="converter-history__item">
+              <p className="converter-history__date">25.11.2020</p>
+              <p className="converter-history__start-sum">1000 RUB</p>
+              <svg className="converter-history__item-icon" width="41" height="18" viewBox="0 0 41 18" fill="none">
+                <path d="M27.2 17L40 9L27.2 1" stroke="#1F1E25"/>
+                <path d="M40 9C34.6667 9 15.7407 9 6.66667 9H0" stroke="#1F1E25"/>
+              </svg>
+              <p className="converter-history__result-sum">13,1234 USD</p>
+            </li>
+            <li className="converter-history__item">
+              <p className="converter-history__date">25.11.2020</p>
+              <p className="converter-history__start-sum">1000 RUB</p>
+              <svg className="converter-history__item-icon" width="41" height="18" viewBox="0 0 41 18" fill="none">
+                <path d="M27.2 17L40 9L27.2 1" stroke="#1F1E25"/>
+                <path d="M40 9C34.6667 9 15.7407 9 6.66667 9H0" stroke="#1F1E25"/>
+              </svg>
+              <p className="converter-history__result-sum">13,1234 USD</p>
+            </li>
+          </ul>
+        </div>
+        <button type="button" className="converter-history__clear-button">
+          Очистить историю
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export {ConverterHistory};
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import {Promo} from '../promo/promo';
+import {ConverterForm} from '../converter-form/converter-form';
+import {ConverterHistory} from '../converter-history/converter-history';
 
-import './main';
+import './main.scss';
 
 const Main = () => {
   return (
     <main className="converter page-body__main">
       <Promo />
+      <ConverterForm />
+      <ConverterHistory />
     </main>
   );
 };
